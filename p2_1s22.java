9e9899df374fbba8a87118852c00b130259b8729
DulceAraceliMondragonSanchez <144294155+DulceAraceliMondragonSanchez@users.noreply.github.com>
2023-09-06 18:57:18 -0600

    Add files via upload

---
+++ p2_1s22.java
@@ -0,0 +1,9 @@
+class p2_1s22 {
+	public static void main(String[] args) {
+		System.out.println("Dulce Araceli Mondragon Sanchez");
+		System.out.println("18 años");
+		System.out.println("dulce19062005sanchez@gmail.com");
+		System.out.println("bailar");
+		System.out.println("lila");
+	}
+}
